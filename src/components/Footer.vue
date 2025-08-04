44c6ae9d39f3a99eb057f6bc9f4f3c376d807709
JustJabbaaaa <adriankotowski217@gmail.com>
2025-08-04 16:50:03 +0200

    first commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer class="footer">
+    <div class="footer__body">
+      Copyright © {{new Date().getFullYear()}} All rights reserved | <NuxtLink to="https://kdproject.pl" aria-label="KDProject.pl - portfolio">KDProject.pl</NuxtLink> 
+    </div>
+  </footer>
+</template>
