ead9f5c0fbe2826b30732f435df9e96fcb51a760
JustJabbaaaa <adriankotowski217@gmail.com>
2025-08-04 21:30:03 +0200

    ahaok

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,25 +3,30 @@
         <div class="about">
             <div class="about__content">
                 <div class="about__content--header">
-                    There are people who don’t seek the spotlight –
-                    they create impact quietly, steadily, with integrity
-                    and responsibility.
+                    <span>
+                        There are people who don’t seek the spotlight –
+                        they create impact quietly, steadily, with integrity
+                        and responsibility.
+                    </span>
                 </div>
-                <div class="about__content--description">
-                    <p>
-                        Born and raised is Oranienburg, deeply rooted in his hometown,
-                        Heiko has earned the trust and respect of the community
-                        over decades – not through consistent action. Through reliability.
-                        Through closeness to the people.
-                    </p>
-                    <p>
-                        For more than 25 years, Heiko Zillmann has been active as
-                        an entrepreneur – in real estate, HR services, and the service
-                        sector. He built companies, created jobs, connected people,
-                        and provided solutions where others only saw problems.
-                        His perspective has always been grounded: focused on what’s
-                        practically achievable – and morally right
-                    </p>
+                <div class="about__content--imgText">
+                    <NuxtImg src="/images/about/about_photo.webp"/>
+                    <div class="about__content--description">
+                        <p>
+                            Born and raised is Oranienburg, deeply rooted in his hometown,
+                            Heiko has earned the trust and respect of the community
+                            over decades – not through consistent action. Through reliability.
+                            Through closeness to the people.
+                        </p>
+                        <p>
+                            For more than 25 years, Heiko Zillmann has been active as
+                            an entrepreneur – in real estate, HR services, and the service
+                            sector. He built companies, created jobs, connected people,
+                            and provided solutions where others only saw problems.
+                            His perspective has always been grounded: focused on what’s
+                            practically achievable – and morally right
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
