e407f30169f5f408a60cb343cfbe09948a240bf5
JustJabbaaaa <adriankotowski217@gmail.com>
2025-08-15 12:44:01 +0200

    gfd

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <div id="Navbar">
     <div class="nav">
       <NuxtLink to="/" class="nav__brand" aria-label="Go to homepage">
-        {{ $t('navbar.logo.yourCandidate') }}<br/>{{ $t('navbar.logo.mayor') }}<br class="linebreak" v-if="$i18n.locale === 'de'" /><NuxtImg src="/images/vision/herb.webp"/><span>Oranienburg</span></NuxtLink>
+        {{ $t('navbar.logo.yourCandidate') }}<br/>{{ $t('navbar.logo.mayor') }}<br class="linebreak" v-if="$i18n.locale === 'de'" /> <NuxtImg src="/images/vision/herb.webp"/><span>Oranienburg</span></NuxtLink>
       <div class="nav__buttons">
         <div class="nav__buttons--navMenu">
           <li class="nav__buttons--item"><NuxtLink to="#About" class="nav__buttons--btn">{{ $t('navbar.links.about') }}</NuxtLink></li>
